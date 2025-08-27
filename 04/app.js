58a9dfebc81cf8e39e14d27c39b952b7d65ee280
Natalia Kur <natalia.kur@yahoo.com>
2025-08-27 20:51:26 +0200

    JS Objects: Complete task 04

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,24 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+  firstName: "Adam",
+  lastName: "Nowak",
+  born: {
+    day: "14",
+    month: "04",
+    year: "1985",
+  },
+};
+
+const today = new Date();
+const formattedDay = today.getDate();
+const formattedMonth = today.getMonth() + 1;
+const userBirthDay = Number(user.born.day);
+const userBirthMonth = Number(user.born.month);
+
+function checkBirthday(user) {
+  if (userBirthDay === formattedDay && userBirthMonth === formattedMonth) {
+    console.log(`Yaay! ${user.firstName} It's your birthday! 🎉`);
+  } else {
+    console.log(`Sorry ${user.firstName}, today is not your birthday 🥲`);
+  }
 }
+checkBirthday(user);
